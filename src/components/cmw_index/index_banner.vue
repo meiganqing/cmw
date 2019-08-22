b0567c036d42dc8285606d0c8810082c61cafe83
zlb <zlb1045456368@163.com>
2019-08-22 17:48:10 +0800

    cm

---
+++ src/components/cmw_index/index_banner.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="hello">
+  <van-swipe :autoplay="3000">
+  <van-swipe-item v-for="(image, index) in images" :key="index">
+    <img v-lazy="image" />
+  </van-swipe-item>
+</van-swipe>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'index_banner',
+  props: {
+  
+  },
+   data() {
+    return {
+      images: [
+        'https://img.yzcdn.cn/vant/apple-1.jpg',
+        'https://img.yzcdn.cn/vant/apple-2.jpg'
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+.hello{
+  width: 100%;
+  height: 200px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import store from './store'
+import Vant from 'vant';
+import 'vant/lib/index.css';
+import { Swipe, SwipeItem } from 'vant';
+import { Lazyload } from 'vant';
 
 Vue.config.productionTip = false
+Vue.use(Vant);
+Vue.use(Swipe).use(SwipeItem);
+Vue.use(Lazyload);
 
 new Vue({
-  render: h => h(App),
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
